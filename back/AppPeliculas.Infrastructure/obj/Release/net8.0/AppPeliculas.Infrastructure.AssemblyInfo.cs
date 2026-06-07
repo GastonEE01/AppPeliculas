ec11be8d7b67229f5379de8c5ecee0a510b8b3fa
GastonEE01 <142426074+GastonEE01@users.noreply.github.com>
2026-06-07 14:51:54 -0300

    Remove exposed API key

--- back/AppPeliculas.Infrastructure/obj/Release/net8.0/AppPeliculas.Infrastructure.AssemblyInfo.cs
+++ back/AppPeliculas.Infrastructure/obj/Release/net8.0/AppPeliculas.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppPeliculas.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ff2e04345bdc7cfa2ebf8806a4811263cc2e72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f683f707ecb79254aab575dad280783f773fc7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppPeliculas.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppPeliculas.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
